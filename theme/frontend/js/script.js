f43fb0fc7acf03335cb948b4e9f64ac8fd29b72a
dung0811 <103187128+dung2511@users.noreply.github.com>
2024-04-23 17:58:08 +0700

    update

--- theme/frontend/js/script.js
+++ theme/frontend/js/script.js
@@ -118,12 +118,68 @@ var GUI = (function () {
             }
         });
     }
+    var moduleSearch = () => {
+        var btnShowSearch = document.querySelectorAll('.btn-show-modal');
+        var btnCloseSearch = document.querySelector(".close-form-search");
+        // var search = document.querySelector('.search-mobile');
+        if (btnShowSearch.length > 0) {
+            btnShowSearch.forEach((btn) => {
+                btn.addEventListener('click', function () {
+                    document.querySelector(".form-search").classList.add("active");
+                })
+            })
+        }
+        if (btnCloseSearch) {
+            btnCloseSearch.addEventListener('click', function () {
+                document.querySelector(".form-search").classList.remove("active");
+            })
+        }
+        // if (search) {
+        //     var width_ = window.innerWidth;
+        //     if (width_ < 1024) {
+        //         var headerHeight = document.querySelector('.header').offsetHeight;
+        //         search.style.top = "calc(" + headerHeight + "px + 1rem)";
+        //     }
+        // }
+    }
+    var backToTop = function () {
+        var backToTopButton = document.querySelector('.btn-to-top');
+        if (backToTopButton) {
+            window.addEventListener('scroll', function () {
+                if (window.scrollY > 600) {
+                    backToTopButton.style.display = 'flex';
+                } else {
+                    backToTopButton.style.display = 'none';
+                }
+            });
+            backToTopButton.addEventListener('click', function () {
+                var scrollOptions = {
+                    top: 0,
+                    behavior: 'smooth'
+                };
+                if ('scrollBehavior' in document.documentElement.style) {
+                    window.scrollTo(scrollOptions);
+                } else {
+                    var scrollInterval = setInterval(function () {
+                        if (window.scrollY <= 0) {
+                            clearInterval(scrollInterval);
+                        } else {
+                            window.scrollBy(0, -20);
+                        }
+                    }, 16);
+                }
+                return false;
+            });
+        }
+    };
     return {
         _: function () {
             showMenu();
             loadmap();
             iconMenu();
             moduleTabContent();
+            moduleSearch();
+            backToTop();
         },
     };
 })();
@@ -131,106 +187,105 @@ var GUI = (function () {
 document.addEventListener("DOMContentLoaded", function () {
     GUI._();
 })
-var CHEAT_TIMER = {
-    init: () => {
-        var times = jQuery('.cmsmasters_counters.counters_type_horizontal .cmsmasters_counter_wrap');
-        var tmps = {};
-        for (var i = 0; i < times.length; i++) {
-            var t = jQuery(times[i]);
-            var value = t.find('.cmsmasters_counter').attr('data-percent').toLowerCase();
-            var title = t.find('.cmsmasters_counter_title').text().toLowerCase();
-            tmps[title] = value;
-        }
+// var CHEAT_TIMER = {
+//     init: () => {
+//         var times = jQuery('.cmsmasters_counters.counters_type_horizontal .cmsmasters_counter_wrap');
+//         var tmps = {};
+//         for (var i = 0; i < times.length; i++) {
+//             var t = jQuery(times[i]);
+//             var value = t.find('.cmsmasters_counter').attr('data-percent').toLowerCase();
+//             var title = t.find('.cmsmasters_counter_title').text().toLowerCase();
+//             tmps[title] = value;
+//         }
 
-        var year = tmps['year'] ?? 2023;
-        var month = tmps['month'] - 1 ?? 0;
-        var day = tmps['day'] ?? 0;
-        var hour = tmps['hour'] ?? 0;
-        var minute = tmps['minute'] ?? 0;
-        var second = tmps['second'] ?? 0;
-        var d = new Date(year, month, day, hour, minute, second);
-        setInterval(() => {
-            var dif = (d.getTime() - (new Date()).getTime()) / 1000;
-            var dx = CHEAT_TIMER.secondsToDhms(dif);
-            var roundSecond = Math.round(dx.seconds).toString();
-            if (roundSecond.length < 2) {
-                roundSecond = "0" + Math.round(dx.seconds);
-            }
-            var roundDay = Math.round(dx.days).toString();
-            if (roundDay.length < 2) {
-                roundDay = "0" + Math.round(dx.days);
-            }
-            var roundHours = Math.round(dx.hours).toString();
-            if (roundHours.length < 2) {
-                roundHours = "0" + Math.round(dx.hours);
-            }
-            var roundMinute = Math.round(dx.minutes).toString();
-            if (roundMinute.length < 2) {
-                roundMinute = "0" + Math.round(dx.minutes);
-            }
-            var str = `<div class="cmsmasters_counter_wrap one_fifth">
-                <div id="cmsmasters_counter_day" class="cmsmasters_counter" data-percent="${Math.round(dx.days)}">
-                    <div class="cmsmasters_counter_inner">
-                        <span class="cmsmasters_counter_counter_wrap">
-                            <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${Math.round(dx.days)}</span><span class="cmsmasters_counter_suffix"></span>
-                        </span>
-                        <span class="cmsmasters_counter_title">DAY</span>
-                    </div>
-                </div>
-            </div>
-            <div class="cmsmasters_counter_wrap one_fifth">
-                <div id="cmsmasters_counter_hour" class="cmsmasters_counter" data-percent="${Math.round(dx.hours)}">
-                    <div class="cmsmasters_counter_inner">
-                        <span class="cmsmasters_counter_counter_wrap">
-                        <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${Math.round(dx.hours)}</span><span class="cmsmasters_counter_suffix"></span>
-                        </span>
-                        <span class="cmsmasters_counter_title">HOUR</span>
-                    </div>
-                </div>
-            </div>
-            <div class="cmsmasters_counter_wrap one_fifth">
-                <div id="cmsmasters_counter_minute" class="cmsmasters_counter" data-percent="${Math.round(dx.minutes)}">
-                    <div class="cmsmasters_counter_inner">
-                        <span class="cmsmasters_counter_counter_wrap">
-                        <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${Math.round(dx.minutes)}</span><span class="cmsmasters_counter_suffix"></span>
-                        </span>
-                        <span class="cmsmasters_counter_title">MINUTE</span>
-                    </div>
-                </div>
-            </div>
-            <div class="cmsmasters_counter_wrap one_fifth">
-                <div id="cmsmasters_counter_second" class="cmsmasters_counter" data-percent="${Math.round(dx.seconds)}">
-                    <div class="cmsmasters_counter_inner">
-                        <span class="cmsmasters_counter_counter_wrap">
-                        <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${roundSecond}</span><span class="cmsmasters_counter_suffix"></span>
-                        </span>
-                        <span class="cmsmasters_counter_title">SECOND</span>
-                    </div>
-                </div>
-            </div>`
-            jQuery('.cmsmasters_counters.counters_type_horizontal').html(str);
-        }, 1000)
+//         var year = tmps['year'] ?? 2023;
+//         var month = tmps['month'] - 1 ?? 0;
+//         var day = tmps['day'] ?? 0;
+//         var hour = tmps['hour'] ?? 0;
+//         var minute = tmps['minute'] ?? 0;
+//         var second = tmps['second'] ?? 0;
+//         var d = new Date(year, month, day, hour, minute, second);
+//         setInterval(() => {
+//             var dif = (d.getTime() - (new Date()).getTime()) / 1000;
+//             var dx = CHEAT_TIMER.secondsToDhms(dif);
+//             var roundSecond = Math.round(dx.seconds).toString();
+//             if (roundSecond.length < 2) {
+//                 roundSecond = "0" + Math.round(dx.seconds);
+//             }
+//             var roundDay = Math.round(dx.days).toString();
+//             if (roundDay.length < 2) {
+//                 roundDay = "0" + Math.round(dx.days);
+//             }
+//             var roundHours = Math.round(dx.hours).toString();
+//             if (roundHours.length < 2) {
+//                 roundHours = "0" + Math.round(dx.hours);
+//             }
+//             var roundMinute = Math.round(dx.minutes).toString();
+//             if (roundMinute.length < 2) {
+//                 roundMinute = "0" + Math.round(dx.minutes);
+//             }
+//             var str = `<div class="cmsmasters_counter_wrap one_fifth">
+//                 <div id="cmsmasters_counter_day" class="cmsmasters_counter" data-percent="${Math.round(dx.days)}">
+//                     <div class="cmsmasters_counter_inner">
+//                         <span class="cmsmasters_counter_counter_wrap">
+//                             <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${Math.round(dx.days)}</span><span class="cmsmasters_counter_suffix"></span>
+//                         </span>
+//                         <span class="cmsmasters_counter_title">DAY</span>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div class="cmsmasters_counter_wrap one_fifth">
+//                 <div id="cmsmasters_counter_hour" class="cmsmasters_counter" data-percent="${Math.round(dx.hours)}">
+//                     <div class="cmsmasters_counter_inner">
+//                         <span class="cmsmasters_counter_counter_wrap">
+//                         <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${Math.round(dx.hours)}</span><span class="cmsmasters_counter_suffix"></span>
+//                         </span>
+//                         <span class="cmsmasters_counter_title">HOUR</span>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div class="cmsmasters_counter_wrap one_fifth">
+//                 <div id="cmsmasters_counter_minute" class="cmsmasters_counter" data-percent="${Math.round(dx.minutes)}">
+//                     <div class="cmsmasters_counter_inner">
+//                         <span class="cmsmasters_counter_counter_wrap">
+//                         <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${Math.round(dx.minutes)}</span><span class="cmsmasters_counter_suffix"></span>
+//                         </span>
+//                         <span class="cmsmasters_counter_title">MINUTE</span>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div class="cmsmasters_counter_wrap one_fifth">
+//                 <div id="cmsmasters_counter_second" class="cmsmasters_counter" data-percent="${Math.round(dx.seconds)}">
+//                     <div class="cmsmasters_counter_inner">
+//                         <span class="cmsmasters_counter_counter_wrap">
+//                         <span class="cmsmasters_counter_prefix"></span><span class="cmsmasters_counter_counter">${roundSecond}</span><span class="cmsmasters_counter_suffix"></span>
+//                         </span>
+//                         <span class="cmsmasters_counter_title">SECOND</span>
+//                     </div>
+//                 </div>
+//             </div>`
+//             jQuery('.cmsmasters_counters.counters_type_horizontal').html(str);
+//         }, 1000)
 
-    },
-    secondsToDhms: (seconds) => {
-        const days = Math.floor(seconds / (3600 * 24));
-        const hours = Math.floor((seconds % (3600 * 24)) / 3600);
-        const minutes = Math.floor((seconds % 3600) / 60);
-        const remainingSeconds = seconds % 60;
+//     },
+//     secondsToDhms: (seconds) => {
+//         const days = Math.floor(seconds / (3600 * 24));
+//         const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+//         const minutes = Math.floor((seconds % 3600) / 60);
+//         const remainingSeconds = seconds % 60;
 
-        return {
-            days: days,
-            hours: hours,
-            minutes: minutes,
-            seconds: remainingSeconds
-        };
-    }
+//         return {
+//             days: days,
+//             hours: hours,
+//             minutes: minutes,
+//             seconds: remainingSeconds
+//         };
+//     }
 
 
-};
-jQuery(document).ready(function () {
-    setTimeout(() => {
-        CHEAT_TIMER.init();
-    }, 1000)
-
-});
+// };
+// document.addEventListener("DOMContentLoaded", function () {
+//     setTimeout(() => {
+//         CHEAT_TIMER.init();
+//     }, 1000)
+// })
